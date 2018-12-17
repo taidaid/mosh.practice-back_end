f59af3a1626e4c420057a02092a0c869618a5daf
Bryan <windsor.bryan@gmail.com>
2018-12-17 22:20:21 +0100

    moving store to back-end

--- server.js
+++ server.js
@@ -5,40 +5,43 @@ const cors = require("cors");
 const app = express();
 
 const database = {
-	counters: [
-		{ id: 1, value: 0 },
-		{ id: 2, value: 2 },
-		{ id: 3, value: 3 },
-		{ id: 4, value: 4 }
-	],
-	store: [
-		{
-			id: 1,
-			img: "https://via.placeholder.com/600/92c952",
-			title: "accusamus beatae ad facilis cum similique qui sunt"
-		},
-		{
-			id: 2,
-			img: "https://via.placeholder.com/600/92c952",
-			title: "accusamus beatae ad facilis cum similique qui sunt"
-		},
-		{
-			id: 3,
-			img: "https://via.placeholder.com/600/92c952",
-			title: "accusamus beatae ad facilis cum similique qui sunt"
-		}
-	]
+  counters: [
+    { id: 1, value: 0 },
+    { id: 2, value: 2 },
+    { id: 3, value: 3 },
+    { id: 4, value: 4 }
+  ],
+  store: [
+    {
+      id: 1,
+      img: "https://via.placeholder.com/600/92c952",
+      title: "accusamus beatae ad facilis cum similique qui sunt"
+    },
+    {
+      id: 2,
+      img: "https://via.placeholder.com/600/92c952",
+      title: "accusamus beatae ad facilis cum similique qui sunt"
+    },
+    {
+      id: 3,
+      img: "https://via.placeholder.com/600/92c952",
+      title: "accusamus beatae ad facilis cum similique qui sunt"
+    }
+  ]
 };
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.json(database.store);
+  //   res.json("it is working");
+  const store = database.store;
+  res.setHeader("Content-Type", "application/json");
+  res.json({ store });
 });
 
 app.listen(3000, () => {
-	console.log("app is running on port 3000");
+  console.log("app is running on port 3000");
 });
 
 /* 
